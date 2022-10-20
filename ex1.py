ac9422b8b192129e01ba61f8777dac21071319e5
Medialive Tigran <medialivetigran@MacBook-Air.local>
2022-10-20 09:54:19 +0400

    ex1 commented

--- ex1.py
+++ ex1.py
@@ -1,27 +1,23 @@
+#loop for inputing and validating text
 while True:
     text = input('Please, enter some text\n')
     if len(text) == 0:
         print("Sorry, I didn't understand that.")
-        #better try again... Return to the start of the loop
     else:
-        #text was successfully validated!
-        #we're ready to exit the loop.
         break
 
+#loop for inputing and validating numer
 while True:
     try:
         num = int(input('Please, enter a number for repetition\n'))
     except ValueError:
         print("Sorry, your value is not a number")
-        #better try again... Return to the start of the loop
         continue
     else:
-        #age was successfully parsed!
-        #we're ready to exit the loop.
         break
 
 result = ''
-
+#adding texts to result as wanted
 for i in range(num):
     result+=text+' '
 
