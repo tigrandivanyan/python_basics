7415932536c5b1e8f5087df3d45e6a79bb4659a8
Medialive Tigran <medialivetigran@MacBook-Air.local>
2022-10-20 10:08:58 +0400

    ex3 added

---
+++ ex3.py
@@ -0,0 +1,15 @@
+#loop for inputing and validating numer
+while True:
+    numText = input('Please, enter a number for repetition\n')
+    if not numText.isnumeric():
+        print("Sorry, your value is not a number")
+        continue
+    else:
+        break
+
+sum = 0
+#calculating the sum by converting each char to int
+for character in numText:
+    sum += int(character)
+
+print(sum)
